7354a08cf238345c66f85de3c39c27a5a24c0da4
Mat Schaffer <mat@schaffer.me>
2012-01-09 07:47:26 -0500

    Fix clojure version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject insane-noises "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [overtone "0.6.0"]])
